c9b381c7274ff2f3f8b59de3a8c1243be25577dc
tizian00 <tizianhandler@gmail.com>
2025-11-10 08:01:27 +0100

    füge Thread-Logik für Zählen in zwei Richtungen hinzu

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.ExceptionServices;
 using System.Threading;
 
 namespace A1_ZweiThreadsZaehlenWinner;
@@ -10,17 +11,31 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+        threadA.Start();
+        threadB.Start();
+        threadA.Join();
+        threadB.Join();
+        Console.WriteLine("Fertig");
         
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine($"Thread A: {i}");
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+       for (int i = 100; i >= 1; i--)
+        {
+            Console.WriteLine($"Thread B: {i}");
+            Thread.Sleep(100);
+        }
     }
 }
